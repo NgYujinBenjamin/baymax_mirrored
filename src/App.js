b6e5fb59e4878d31a9ff46a0b2a1528da66039ce
yazidmhd <accelyazid@gmail.com>
2019-12-29 14:36:39 +0800

    setup routes to navigate through pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core';
+
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Baseline from './components/linearization/Baseline';
+import Home from './components/pages/Home';
+
 import UploadState from './context/upload/UploadState';
 import AlertState from './context/alert/AlertState';
 
@@ -11,13 +14,17 @@ const App = () => {
   return (
     <UploadState>
     <AlertState>
-      <div className='App'>
-        <Navbar />
-        <Container>
-          <Alert />
-          <Baseline />
-        </Container>
-      </div>
+      <Router>
+        <Fragment>
+          <Navbar />
+            <Container>
+              <Alert />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                </Switch>
+            </Container>
+        </Fragment>
+      </Router>
     </AlertState>  
     </UploadState>
   );
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Baseline from '../linearization/Baseline';
+
+const Home = () => {
+    return (
+        <Fragment>
+            <Baseline />
+        </Fragment>
+    )
+}
+
+export default Home
