a94509b9d73c40d5f68c2f50d75c55df488d3bf3
Benjamin <benjaminng.2017@sis.smu.edu.sg>
2020-02-10 11:17:19 +0800

    minor changes

---
+++ windows.bat
@@ -0,0 +1,7 @@
+c:\
+cd C:\Users\XPS 15\Desktop\IS Project Experience\Git Second Attempt\baymax\springboot
+start mvnw spring-boot:run
+timeout 5
+c:\
+cd C:\Users\XPS 15\Desktop\IS Project Experience\Git Second Attempt\baymax
+start npm start
