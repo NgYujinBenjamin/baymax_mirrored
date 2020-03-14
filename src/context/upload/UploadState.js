c62e91e7ca8983fda27e1a87f27dbd4b37fc08ef
Celine <celinetan2009@hotmail.com>
2020-03-14 22:34:41 +0800

    Update the conditions of endDate function

--- src/context/upload/UploadState.js
+++ src/context/upload/UploadState.js
@@ -692,8 +692,13 @@ const UploadState = (props) => {
     const endDateCheck = (qtrObj, key, minGap) => {
       let intRedDate = dateConversion(qtrObj.intOpsShipReadinessDate);
       let MFGCommit = dateConversion(qtrObj.MFGCommitDate);
-      {qtrObj.fabName == "OPEN" ? 
-      qtrObj[key] = new Date(intRedDate.setTime(intRedDate.getTime() - minGap)).toLocaleDateString('en-GB') : qtrObj[key] = new Date(MFGCommit.setTime(MFGCommit.getTime() - minGap)).toLocaleDateString('en-GB');
+      
+      if (qtrObj.lockMRPDate){ // if true, endDate == MRPDate
+        qtrObj[key] = qtrObj.MRPDate;
+      } else if (qtrObj.fabName == "OPEN"){ // if OPEN status, endDate == IRR - mingap
+        qtrObj[key] = new Date(intRedDate.setTime(intRedDate.getTime() - minGap)).toLocaleDateString('en-GB')
+      } else { // normal situation - endDate == MFG - mingap
+        qtrObj[key] = new Date(MFGCommit.setTime(MFGCommit.getTime() - minGap)).toLocaleDateString('en-GB');
       }
     }
 
