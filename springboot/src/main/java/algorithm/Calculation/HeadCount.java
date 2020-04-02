c48704850e76b08f4d883fc15d5ac891bcbf1824
NG Pok Hei Bryan <bryan.ng.2016@accountancy.smu.edu.sg>
2020-04-02 21:56:20 +0800

    Update to AlgoControllerTest, Product, HeadCount
    - HeadCount Unit Testing
    - Update to HeadCount calculation for current quarter
    - Product error/exception messages

--- springboot/src/main/java/algorithm/Calculation/HeadCount.java
+++ springboot/src/main/java/algorithm/Calculation/HeadCount.java
@@ -9,6 +9,12 @@ import com.google.gson.Gson;
 public class HeadCount{
     private HashMap<String, Integer> quarterHC;
     
+    /**
+     * Calculates the quarterly headcount available to start building a new product.
+     * In the current quarter, this is calculated by the number of outstanding jobs in the quarter/ number of weeks left in quarter 
+     * In a full quarter, this is calculated by number of jobs in the quarter/ 12, where 12 is the rough estimate of the number of weeks in a quarter
+     * @param allProduct Representing all future products to be built in the quarter
+     */
     public HeadCount(ArrayList<Product> allProduct){
         HashMap<String, Integer> quarterJobs = new HashMap<String, Integer>();
         quarterHC = new HashMap<String, Integer>();
@@ -24,14 +30,80 @@ public class HeadCount{
             quarterJobs.put(buildQtr, newBuildCount);
         }
         
+        String currQuarter = getCurrentQtr();
         Set<String> quarters = quarterJobs.keySet();
         for(String q: quarters){
             Integer numQtrJobs = quarterJobs.get(q); 
-            Integer numHeadCount = (int) Math.ceil((double) numQtrJobs/12);
+            Integer numHeadCount = null;
+            if (q.equals(currQuarter)) {
+            	int currQtrRemWeeks = getCurrentQtrRemWeeks();
+            	numHeadCount = (int) Math.ceil((double) numQtrJobs/currQtrRemWeeks);
+            } else {
+            	// Subsequent Quarters
+            	numHeadCount = (int) Math.ceil((double) numQtrJobs/12);
+            } 
             quarterHC.put(q, numHeadCount);
         }
     }
-
+    
+    /**
+     * Helper method to identify the current quarter based on the today's date
+     * @return Current Quarter in the form of "CY_ _Q _" (e.g CY20Q1)
+     */
+    private String getCurrentQtr(){
+    	String quarter = "";
+    	
+    	Date today = new Date();
+    	Integer year = today.getYear() - 100;
+    	quarter += "CY" + year.toString();
+    	
+    	int month = today.getMonth();
+    	if (month <= 2) {
+    		quarter += "Q1";
+    	} else if (month <= 5) {
+    		quarter += "Q2";
+    	} else if (month <= 8) {
+    		quarter += "Q3";
+    	} else {
+    		quarter += "Q4";
+    	}
+    	return quarter;
+    }
+    
+    /**
+     * Helper method to calculate the number of weeks remaining in the quarter, from today's date
+     * @return Number of weeks remaining in the quarter
+     */
+    private int getCurrentQtrRemWeeks(){
+    	Date quarterEnd = null;
+    	
+    	Date today = new Date();
+    	int year = today.getYear();
+    	int month = today.getMonth();
+    	
+    	if (month <= 2) {
+    		// Mar 31st
+    		quarterEnd = new Date(year, 2, 31);
+    	} else if (month <= 5) {
+    		// June 30
+    		quarterEnd = new Date(year, 5, 30);
+    	} else if (month <= 8) {
+    		// Sep 30
+    		quarterEnd = new Date(year, 8, 30);
+    	} else {
+    		// Dec 31
+    		quarterEnd = new Date(year, 1, 31);
+    	}
+    	
+    	int remWeeks = (int) ((quarterEnd.getTime() - today.getTime())/ (7* 24 * 60 * 60 * 1000));
+    	
+    	return remWeeks;
+    }
+    
+    /**
+     * Returns Headcount availablility for all build quarters in the masterOps products
+     * @return Returns Headcount availability for all build quarters in the masterOps products
+     */
     public HashMap<String, Integer> getQuarterHC(){
         return quarterHC;
     }
