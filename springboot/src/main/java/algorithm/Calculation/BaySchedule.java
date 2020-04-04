1f4f79f96fdbffb8094ca30c7821da0493d2c956
NG Pok Hei Bryan <bryan.ng.2016@accountancy.smu.edu.sg>
2020-04-05 01:46:47 +0800

    Update to Algo Controller due to corner case

--- springboot/src/main/java/algorithm/Calculation/BaySchedule.java
+++ springboot/src/main/java/algorithm/Calculation/BaySchedule.java
@@ -160,7 +160,7 @@ public class BaySchedule{
     private Date adjustToolStart(Date latestToolStartDate, Date bayAvailDate, Integer gapDiff){
         Date earliestToolStartDate;
         Date today = new Date();
-        // Date today = new Date(120, 1, 25); // For testing purposes
+        // Date today = new Date(120, 2, 24); // For testing purposes
         if (bayAvailDate.after(today)){
             // If bayAvailableDate is after current date, we can pull forward up to the maxGap or the date when the bay is available
             Long diff = latestToolStartDate.getTime() - bayAvailDate.getTime();
